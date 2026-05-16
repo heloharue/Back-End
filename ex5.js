2415aa2f176ee948d2e8fc126d6b6776ffd5eadd
Harue <heloisa.ussami.goncalves@escola.pr.gov.br>
2026-05-16 19:52:31 -0300

    update

--- ex5.js
+++ ex5.js
@@ -4,11 +4,16 @@ let par = 0;
 let impar = 0;
 
 for(let i=0; i<10; i++){
-    let num = input.question("digite o primeito numero: ");
+    let num = Number(input.question("digite o primeito numero: "));
 
-    if(num % 2 === 0){
+    if(num%2 === 0){
+        par++;
         console.log("o numero é par" , par);
     }else{
+        impar++;
         console.log("o numero é impar");
     }
 }
+
+console.log("os numeros pares foram " , par);
+console.log("os numeros impares foram " , impar);
