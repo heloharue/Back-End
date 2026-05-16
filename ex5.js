af1690d124a0021d096439ec57dc954c6040a9bd
Harue <heloisa.ussami.goncalves@escola.pr.gov.br>
2026-05-16 19:41:46 -0300

    update

--- ex5.js
+++ ex5.js
@@ -1,1 +1,14 @@
 const input = require("readline-sync");
+
+let par = 0;
+let impar = 0;
+
+for(let i=0; i<10; i++){
+    let num = input.question("digite o primeito numero: ");
+
+    if(num % 2 === 0){
+        console.log("o numero é par" , par);
+    }else{
+        console.log("o numero é impar");
+    }
+}
