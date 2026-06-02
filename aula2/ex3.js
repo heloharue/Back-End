5309310b71a3a833838170bea3103a3bc722c685
Harue <heloisa.ussami.goncalves@escola.pr.gov.br>
2026-06-02 17:53:30 -0300

    Update ex3.js

--- aula2/ex3.js
+++ aula2/ex3.js
@@ -5,7 +5,7 @@ const sub = (num1, num2) => num1-num2;
 const mult = (num1, num2) => num1*num2;
 const div = (num1, num2) => {
     if(num2 === 0) {
-        return "Erro: divisao por zero";
+        return "Numero invalido, escolha outro";
     } else {
         return num1/num2;
     }
